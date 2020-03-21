2a47587d760ddaba67522723bb706a08e8b573f5
Michael Agrapides <michaelagrapides@Michaels-MBP.fios-router.home>
2020-03-21 09:50:27 -0400

    ---

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,9 +1,30 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, wire, api } from 'lwc';
 import getAccountList from '@salesforce/apex/AccountController.getAccountList';
+import findAccount from '@salesforce/apex/AccountController.findAccount';
+const DELAY = 300;
 
 export default class HelloWorld extends LightningElement {
   greeting = 'World';
   changeHandler(event) {this.greeting = event.target.value;}
+
   @wire(getAccountList) accounts;
+
+  @api recordId = '';
+
+  searchKey = '';
+
+  @wire(findAccount, { searchKey: '$searchKey' })
+  recordId;
+
+  handleKeyChange(event) {
+      // Debouncing this method: Do not update the reactive property as long as this function is
+      // being called within a delay of DELAY. This is to avoid a very large number of Apex method calls.
+      window.clearTimeout(this.delayTimeout);
+      const searchKey = event.target.value;
+      this.delayTimeout = setTimeout(() => {
+          this.searchKey = searchKey;
+      }, DELAY);
+  }
+  
 }
     
